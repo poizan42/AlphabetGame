a809a8e87659dfd808ea575d1043af52db702e6b
Kasper F. Brandt <poizan@poizan.dk>
2012-08-08 20:47:16 +0200

    Preliminary locale support, license info and readme file

--- AlphabetGame.py
+++ AlphabetGame.py
@@ -1,6 +1,31 @@
 # -*- coding: utf8 -*-
-# AlphabetGame v. 0.2
-# Copyright (C) Kasper F. Brandt 2009
+# AlphabetGame v. 0.3
+#
+# Copyright (c) Kasper F. Brandt 2009,2012
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import sys
 import time
 
@@ -76,10 +101,32 @@ class _GetchMacCarbon:
 
 getch = _Getch()
 
+locales = {}
+daloc = {
+	'intro0': "Skriv alfabetet fra a-z s\x86 hurtigt som muligt",
+	'intro1': "Start p\x86 a (- for at slutte):",
+	'lost': "Du har tabt spillet!",
+	'won': "Du vandt spillet!",
+	'stattime': "Tid: %.3f s",
+	'statkeystrokes': "Anslag/minut: %.1f\n"
+}
+locales['da'] = daloc
+enloc = {
+	'intro0': "Type the alphabet from a-z as fast as possible",
+	'intro1': "Start with a (- to quit):",
+	'lost': "You have lost the game!",
+	'won': "You have won the game!",
+	'stattime': "Time: %.3f s",
+	'statkeystrokes': "Keystrokes/minute: %.1f\n"
+}
+locales['en'] = enloc
+
+locale = locales['en']
+
 while True:
     fail = False
-    print "Skriv alfabetet fra a-z s\x86 hurtigt som muligt"
-    print "Start p\x86 a (- for at slutte):"
+    print locale['intro0']
+    print locale['intro1']
     pos = 0
     while pos < 26:
         c = getch()
@@ -92,17 +139,17 @@ while True:
             starttime = time.time()
         sys.stdout.write(c)
         if (c != chr(ord('a')+pos)):
-            print "\n\nDu har tabt spillet!"
+            print "\n\n" + locale['lost']
             timeSpan = time.time()-starttime
-            print "Tid: %.3f s" % timeSpan
-            print "Anslag/minut: %.1f\n" % (60*pos/timeSpan)
+            print locale['stattime'] % timeSpan
+            print locale['statkeystrokes'] % (60*pos/timeSpan)
             fail = True
             break
         pos += 1
     if fail:
         continue
 
-    print "\nDu vandt spillet!"
+    print "\n" + locale['won']
     timeSpan = time.time()-starttime
-    print "Tid: %.3f s" % timeSpan
-    print "Anslag/minut: %.1f\n" % (60*26/timeSpan)
+    print locale['stattime'] % timeSpan
+    print locale['statkeystrokes'] % (60*26/timeSpan)
