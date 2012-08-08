5aaa71ef5dcda4e4a79d6e213b90f65e2f1c9d44
Kasper F. Brandt <poizan@poizan.dk>
2012-08-08 20:22:54 +0200

    Initial commit: version 0.2

---
+++ AlphabetGame.py
@@ -0,0 +1,108 @@
+# -*- coding: utf8 -*-
+# AlphabetGame v. 0.2
+# Copyright (C) Kasper F. Brandt 2009
+import sys
+import time
+
+class _Getch:
+    """
+    Gets a single character from standard input.  Does not echo to
+    the screen.
+    """
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            try:
+                self.impl = _GetchMacCarbon()
+            except(AttributeError, ImportError):
+                self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys, termios # import termios now or else you'll get the Unix version on the Mac
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+
+class _GetchMacCarbon:
+    """
+    A function which returns the current ASCII key that is down;
+    if no ASCII key is down, the null string is returned.  The
+    page http://www.mactech.com/macintosh-c/chap02-1.html was
+    very helpful in figuring out how to do this.
+    """
+    def __init__(self):
+        import Carbon
+        Carbon.Evt #see if it has this (in Unix, it doesn't)
+
+    def __call__(self):
+        import Carbon
+        if Carbon.Evt.EventAvail(0x0008)[0]==0: # 0x0008 is the keyDownMask
+            return ''
+        else:
+            #
+            # The event contains the following info:
+            # (what,msg,when,where,mod)=Carbon.Evt.GetNextEvent(0x0008)[1]
+            #
+            # The message (msg) contains the ASCII char which is
+            # extracted with the 0x000000FF charCodeMask; this
+            # number is converted to an ASCII character with chr() and
+            # returned
+            #
+            (what,msg,when,where,mod)=Carbon.Evt.GetNextEvent(0x0008)[1]
+            return chr(msg & 0x000000FF)
+
+
+getch = _Getch()
+
+while True:
+    fail = False
+    print "Skriv alfabetet fra a-z s\x86 hurtigt som muligt"
+    print "Start p\x86 a (- for at slutte):"
+    pos = 0
+    while pos < 26:
+        c = getch()
+        if c == '-':
+            exit(0)
+        if pos == 0 and c != 'a':
+            pos = 0
+            continue
+        if pos == 0:
+            starttime = time.time()
+        sys.stdout.write(c)
+        if (c != chr(ord('a')+pos)):
+            print "\n\nDu har tabt spillet!"
+            timeSpan = time.time()-starttime
+            print "Tid: %.3f s" % timeSpan
+            print "Anslag/minut: %.1f\n" % (60*pos/timeSpan)
+            fail = True
+            break
+        pos += 1
+    if fail:
+        continue
+
+    print "\nDu vandt spillet!"
+    timeSpan = time.time()-starttime
+    print "Tid: %.3f s" % timeSpan
+    print "Anslag/minut: %.1f\n" % (60*26/timeSpan)
